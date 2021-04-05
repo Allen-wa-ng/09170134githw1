4d9663fd4fafbfea7a9fec93137ad01ef9d75115
Allen-wa-ng <wang0911309998@gmail.com>
2021-04-05 21:26:20 +0800

    GAME

--- game.py
+++ game.py
@@ -126,7 +126,7 @@ def draw_lc():
     for i in range(5):
         pygame.draw.lines(screen, color,True, [(75+i*70,150),(75+i*70,650)], 5)
 
-###ß
+
 #RANDOM NUMBER
 #NEXT BLOCK NUMBER
 random.seed()
