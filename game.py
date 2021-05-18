893a7b8982350db3d6a1a6c16176a9e9641423cc
Cangyu Lin <jefflin.je598@gmail.com>
2021-05-18 10:38:59 +0800

    change the name

--- game.py
+++ game.py
@@ -32,7 +32,7 @@ pygame.display.set_caption('2048 V.2') #caption
 
 ### Background music
 pygame.mixer.music.load('let it go.ogg') #let it go.mp3 #mission.mp3
-pygame.mixer.music.set_volume(0.5) #set volume
+pygame.mixer.music.set_volume(0) #set volume
 
 
 ### Set global variable
