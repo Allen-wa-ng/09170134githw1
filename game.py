7a6780a6be6ec1eafafd08f3d5115508f63cc853
Allen-wa-ng <wang0911309998@gmail.com>
2021-05-19 19:25:31 +0800

    n

--- game.py
+++ game.py
@@ -34,7 +34,6 @@ pygame.display.set_caption('2048 V.2') #caption
 pygame.mixer.music.load('let it go.ogg') #let it go.mp3 #mission.mp3
 pygame.mixer.music.set_volume(0.5) #set volume
 
-
 ### Set global variable
 #If mute
 mute = False
